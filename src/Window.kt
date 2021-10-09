1632852f99d9544da16ff35daf7d6a7dc6c83b3f
Vinicius <viniciusalmada10@gmail.com>
2021-10-09 01:15:55 -0300

    first window

--- src/Window.kt
+++ src/Window.kt
@@ -1,7 +1,10 @@
+import java.awt.Dimension
 import javax.swing.JFrame
 
 class Window : JFrame() {
     init {
-
+        defaultCloseOperation = EXIT_ON_CLOSE
+        size = Dimension(640, 480)
+        isVisible = true
     }
 }
